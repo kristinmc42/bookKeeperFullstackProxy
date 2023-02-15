08ce860121bab09e2b6759881e96f8ea08be7277
kristinmc42 <mccollum.k.j@gmail.com>
2023-02-15 13:15:46 -0500

    fixed alignment of clearsearch button for large screens

--- client/src/components/SearchBar.tsx
+++ client/src/components/SearchBar.tsx
@@ -89,6 +89,9 @@ const StyledForm = styled.form`
       border: 1px solid ${(props) => props.theme.colors.primary};
       cursor: pointer;
     }
+    @media ${device.tablet}{
+      align-self: flex-end;
+    }
   }
 
   @media ${device.tablet} {
@@ -96,10 +99,6 @@ const StyledForm = styled.form`
 
     button {
       margin-left: 20px;
-    }
-
-    button.sc-pyfCe:nth-child(3) {
-      align-self: flex-end;
     }
   }
 `;
